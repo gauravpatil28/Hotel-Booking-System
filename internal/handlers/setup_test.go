741378a70c86725037df8369d722b821e29f2024
Gaurav Patil <gauravpatil1628@gmail.com>
2023-04-18 20:08:56 +0530

    test

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -42,6 +42,13 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+
+	app.MailChan = mailChan
+	defer close(mailChan)
+
+	listenForMail()
+
 	tc, err := CreateTestTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -60,6 +67,14 @@ func TestMain(m *testing.M) {
 	repo := NewTestRepo(&app)
 	NewHandlers(repo)
 	render.NewRenderer(&app)
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			_ = <-app.MailChan
+		}
+	}()
 }
 
 func getRoutes() http.Handler {
